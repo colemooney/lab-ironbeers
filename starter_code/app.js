c4787a79bf86608756c583935b1a9b424f7d37c5
colemooney <colemooney5@gmail.com>
2020-08-22 16:54:10 -0400

    home css

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const hbs = require('hbs');
+const app = express();
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
-const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-
+app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// add the routes here
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res, next) => {
+    res.render('index');
+});
+app.get('/beers', (req, res, next) => {
+    res.render('beers');
+});
+app.get('/random-beers', (req, res, next) => {
+    res.render('index');
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
